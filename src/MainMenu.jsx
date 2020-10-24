0f22a8d781ab8b631f71c90fe06e8f7dc7f0f3b2
Thorstein Hagen Ensrud <thensrud@hotmail.com>
2020-10-24 22:06:33 +0200

    Began adding go-to-cart box, and some CSS changes

--- src/MainMenu.jsx
+++ src/MainMenu.jsx
@@ -8,6 +8,7 @@ import Dessert from './Dessert';
 const MainMenu = () => {
 
     const menuSectionNames = ["Varm drikke", "Kald drikke", "Dessert"];
+    let totalPrice = 1;
 
     // Disse to er states her i MainMenu (se "useState"). Inne i useState(her) er det lagt utgangspunkt-verdier.
     // Gjeldende verdi ligger alltid i første variabel i arrayet, og endres når funksjonen (andre del av arrayet) kalles.
@@ -65,8 +66,6 @@ const MainMenu = () => {
             </div>
 
             <div>
-                <h1>{menuSection}</h1>
-
                 {/* Siden laster/menyknapp trykkes på > [menuSection] endres og trigger useEffect > 
                 ett komponent lages per objekt i riktig array i productList.js og lagres som array i [menuItems] > 
                 og denne her rendrer til slutt én per komponent i menuItems */}
@@ -76,6 +75,12 @@ const MainMenu = () => {
                     })}
                 </div>
             </div>
+
+            {totalPrice > 0 &&
+            <div id="total-price-div">
+                <div id="total-price-text-div">Totalpris: <b>{totalPrice} kr,-</b></div>
+                <button id="go-to-shopping-cart-button">Gå til handlekurv</button>
+            </div>}
         </>
     )
 }
