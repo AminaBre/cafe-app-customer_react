9337e65abc1925d26ac312834e1933d0e0110578
Waligustav <wgbjork@gmail.com>
2020-10-29 22:02:53 +0100

    Output to shoppingcart still has cancer

--- src/Views/Shoppingcart.jsx
+++ src/Views/Shoppingcart.jsx
@@ -7,23 +7,22 @@ export const Shoppingcart = (props) => {
   const handleKurv = useContext(HandleKurv);
   return (
     <>
-      <div>{
-        Object.keys(handleKurv.products).forEach((product) => {
+      <div>
+        {Object.keys(handleKurv.products).map((product) => {
           console.log("Vara:  " + product);
-          Object.keys(handleKurv.products[product]).forEach((size) => {
+          Object.keys(handleKurv.products[product]).map((size) => {
             console.log("Storlek:  " + size);
             const amount = handleKurv.products[product][size].antal;
             const aPrice = handleKurv.products[product][size].price;
             console.log("Antal:  " + amount);
             console.log("Pris:  " + aPrice);
+            return <div className="shopping-cart-output">Du har beställt + {(amount, aPrice)}</div>;
           });
         })}
-        <div className="shopping-cart-output">Du har beställt + {amount, aPrice}</div>
       </div>
     </>
   );
       
-  
   // (Object.keys(HandleKurv.products).forEach((product) => {
   //   <div>{product.storlek}</div>
   // }));
