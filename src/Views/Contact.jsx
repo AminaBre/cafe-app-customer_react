dd076a6f44b4c008d08feefc80a7fc4a6a463924
Elise Easter <56083509+eliseeaster@users.noreply.github.com>
2020-11-06 09:18:01 +0100

    Modal works on every page

--- src/Views/Contact.jsx
+++ src/Views/Contact.jsx
@@ -4,20 +4,37 @@ import MainMenu from './Menu/MainMenu';
 import Shoppingcart from './Shoppingcart';
 import '../Styles/styles.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { HandleKurv } from '../Model/handleKurv';
+import { Modal } from '../Modal';
 
 export const Contact = () => {
+
+  const [show, setShow] = useState(false);
+  const closeModalHandler = () => setShow(false);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const [isOpen, setIsOpen] = useState(false);
 
     return(
 
         <>
 
 <header id='header-container'>
+        <div>
+          {show ? <div className="back-drop" onClick={closeModalHandler}></div> : null}
+        </div>
+
         <Link to='/FrontPage/FrontPage' h1 id='header-title' className='font-cursive'>
           Kafé Judas
         </Link>
-        <img id='hamburger-icon' src='../assets/hamburger-icon.png' />
+        <Link to='/FrontPage/FrontPage'>
+          <img id='back-arrow-icon' src='../assets/back-arrow.png' />
+        </Link>
+        
+          <Modal show={show} close={closeModalHandler}/>   
+        <img id='hamburger-icon' src='../assets/hamburger-icon.png' onClick={() => setShow(true)}/>
+
       </header>
+      
         <body>
           <div class="contact-form-container texture-background">
             <div class="contact-form">
