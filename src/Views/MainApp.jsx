ca29e82fad9357afb39bfd439d230da6d3706fb5
Waligustav <wgbjork@gmail.com>
2020-11-07 15:58:27 +0100

    Shoppingcart fixed - correct output

--- src/Views/MainApp.jsx
+++ src/Views/MainApp.jsx
@@ -1,48 +1,43 @@
-  import React, { useState } from 'react';
-  import FrontPage from './FrontPage/FrontPage';
-  import MainMenu from './Menu/MainMenu';
-  import Shoppingcart from './Shoppingcart';
-  import '../Styles/styles.css';
-  import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-  import { HandleKurv } from '../Model/handleKurv';
-import Contact from './Contact';
+import React, { useState } from "react";
+import FrontPage from "./FrontPage/FrontPage";
+import MainMenu from "./Menu/MainMenu";
+import Shoppingcart from "./Shoppingcart";
+import "../Styles/styles.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HandleKurv } from "../Model/handleKurv";
+import Contact from "./Contact";
 
-  const MainApp = (props) => {
+const MainApp = (props) => {
+  const [fontSize, setFontsize] = useState(16);
+  const [products, setProducts] = useState({});
 
-    const [fontSize, setFontsize] = useState(16);
-    const [products, setProducts] = useState({});
+  const allElements = document.getElementById("root");
+  allElements.style.fontSize = `${fontSize}px`;
 
-
-    const allElements = document.getElementById("root");
-    allElements.style.fontSize = `${fontSize}px`;
-
-    return (
-      
-      <Router>
-        <HandleKurv.Provider value={{ products, setProducts }}>
-          {/*
+  return (
+    <Router>
+      <HandleKurv.Provider value={{ products, setProducts }}>
+        {/*
           <header id='header-container'>
             <Link to='/FrontPage/FrontPage' h1 id='header-title' className='font-cursive'>
               Kafé Judas
             </Link>
             <img id='hamburger-icon' src='assets/hamburger-icon.png' />
-
             <div className="font-changer">
               <button onClick = {() => setFontsize(fontSize + 5)}>+ increase</button>
               <button onClick = {() => setFontsize(fontSize - 5)}>- decrease</button>
             </div>
-
           </header>
           */}
-          <Switch>
-            <Route exact path='/MainMenu/:area?' component={MainMenu} />
-            <Route exact path='/ShoppingCart/' component={Shoppingcart} />
-            <Route exact path='/Contact/' component={Contact} />
-            <Route component={FrontPage} />
-          </Switch>
-        </HandleKurv.Provider>
-      </Router>
-    );
-  };
+        <Switch>
+          <Route exact path="/MainMenu/:area?" component={MainMenu} />
+          <Route exact path="/ShoppingCart/" component={Shoppingcart} />
+          <Route exact path="/Contact/" component={Contact} />
+          <Route component={FrontPage} />
+        </Switch>
+      </HandleKurv.Provider>
+    </Router>
+  );
+};
 
-  export default MainApp;
+export default MainApp;
